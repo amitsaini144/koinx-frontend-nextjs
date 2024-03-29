79cf00eaed88a13dfbc5353b02092f143ccc4808
Amit Saini <1amit24saini5@gmail.com>
2024-03-29 20:42:15 +0530

    first commit

---
+++ components/navbar/Logo.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+
+export default function Logo() {
+  return (
+    <Image src="https://app.koinx.com/static/media/Logo.3331aa2fc2f35c00e58921b44a2ebf0d.svg" alt="koinX" className="h-5" />
+  )
+}
---
+++ components/tokenomics/TokenomicsSection.tsx
@@ -0,0 +1,29 @@
+
+
+export default function TokenomicsSection() {
+   
+    return (
+        <div className="bg-white pl-8 pr-10 py-6 rounded-lg shadow-sm mb-10">
+            <h1 className="text-2xl font-semibold mb-5">Tokenomics</h1>
+            <h2 className="text-lg font-semibold mb-2">Initial Distribution</h2>
+            <div className='flex items-center'>
+                <div className='h-36 my-5'>
+
+                </div>
+                <div className='ml-6 text-sm text-black/80 font-medium'>
+                    <div className='flex items-center gap-x-2 mb-1'>
+                        <div className='h-2 w-2 rounded-full bg-cyan-700'></div>
+                        <p>Crowdsale investors: 80%</p>
+                    </div>
+                    <div className='flex items-center gap-x-2'>
+                        <div className='h-2 w-2 rounded-full bg-yellow-700'></div>
+                        <p>Foundation: 20%</p>
+                    </div>
+                </div>
+            </div>
+            <p className="font-medium text-sm text-black/90  mb-7">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ornare congue interdum. Donec non nulla pretium, viverra neque elementum, mollis nisl. Donec auctor sapien at tellus sollicitudin hendrerit. Aenean felis arcu, sodales non mauris sit amet.
+                tempus viverra tortor. Etiam quis blandit magna. Pellentesque cursus nibh id vulputate ultrices. Aliquam id sapien libero. Curabitur vel dolor vitae purus eleifend interdum a ac orci.</p>
+        </div>
+    )
+}
