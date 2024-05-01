7bc78cd241758b4da3fdc12c9c2cd163faa10a10
Amit Saini <1amit24saini5@gmail.com>
2024-05-01 13:17:10 +0530

    updated logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -3,7 +3,6 @@ import logo from "@/public/assets/images/koinx-logo.png"
 
 export default function Logo() {
   return (
-    <Image src={logo} alt="koinX"
-    width={100} height={100}  className="" />
+    <Image src={logo} alt="koinX" className="w-24" />
   )
 }
